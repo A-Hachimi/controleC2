cb38a0a5e3d6e25b22ba946f078bace2a3ea694d
A-Hachimi <Hachimi.abdelilah@yahoo.com>
2020-06-11 15:21:39 +0200

    bootstrap

--- controle2/src/app/app-routing.module.ts
+++ controle2/src/app/app-routing.module.ts
@@ -2,7 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [{ path: 'cars', loadChildren: () => import('./cars/cars.module').then(m => m.CarsModule) }, { path: 'carsdetails', loadChildren: () => import('./cars-details/cars-details.module').then(m => m.CarsDetailsModule) }];
+const routes: Routes = [
+  { 
+  path: 'cars',
+   loadChildren: () => import('./cars/cars.module').then(m => m.CarsModule) },
+  { path: 'carsdetails',
+   loadChildren: () => import('./cars-details/cars-details.module').then(m => m.CarsDetailsModule) }];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- controle2/src/app/cars/cars.component.ts
+++ controle2/src/app/cars/cars.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cars',
@@ -7,7 +8,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CarsComponent implements OnInit {
 
-  constructor() { }
+  cars = [
+    {
+      matricule: "0",
+      marque : "dacia",
+      siege: "fes",
+      color: "red",
+      image: "../../assets/Super_Angular.png",
+    },    
+    {
+      matricule: "1",
+      marque : "fiat",
+      siege: "casablanca",
+      color: "bleu",
+      image: "../../assets/Super_Angular.png",
+    },    
+    {
+      matricule: "2",
+      marque : "fiat",
+      siege: "marakesh",
+      color: "green",
+      image: "../../assets/Super_Angular.png",
+    },    
+    {
+      matricule: "3",
+      marque : "fiat",
+      siege: "taza",
+      color: "black",
+      image: "../../assets/Super_Angular.png",
+    },   
+    {
+      matricule: "4",
+      marque : "mercides",
+      siege: "rabat",
+      color: "white",
+      image: "../../assets/Super_Angular.png",
+    },
+  ];
+
+
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
