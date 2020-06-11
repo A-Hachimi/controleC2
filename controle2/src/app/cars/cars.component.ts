e8030f3800a341672b60d45fc765f0a9c858e622
A-Hachimi <Hachimi.abdelilah@yahoo.com>
2020-06-11 15:29:45 +0200

    cars

--- controle2/src/app/cars/cars.component.ts
+++ controle2/src/app/cars/cars.component.ts
@@ -14,35 +14,35 @@ export class CarsComponent implements OnInit {
       marque : "dacia",
       siege: "fes",
       color: "red",
-      image: "../../assets/Super_Angular.png",
+      image: "../../assets/image.png",
     },    
     {
       matricule: "1",
       marque : "fiat",
       siege: "casablanca",
       color: "bleu",
-      image: "../../assets/Super_Angular.png",
+      image: "../../assets/image.png",
     },    
     {
       matricule: "2",
       marque : "fiat",
       siege: "marakesh",
       color: "green",
-      image: "../../assets/Super_Angular.png",
+      image: "../../assets/image.png",
     },    
     {
       matricule: "3",
       marque : "fiat",
       siege: "taza",
       color: "black",
-      image: "../../assets/Super_Angular.png",
+      image: "../../assets/image.png",
     },   
     {
       matricule: "4",
       marque : "mercides",
       siege: "rabat",
       color: "white",
-      image: "../../assets/Super_Angular.png",
+      image: "../../assets/image.png",
     },
   ];
 
@@ -52,4 +52,7 @@ export class CarsComponent implements OnInit {
   ngOnInit() {
   }
 
+  showDetails(car){
+    this.router.navigate(["product-carsdetails", JSON.stringify(car)]);
+  }
 }
